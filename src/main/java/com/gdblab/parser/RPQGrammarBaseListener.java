ba44342f8b3dc81757e42c6d9ab4d3c49e06dfbd
Vicente Rojas <vrojasaranda@gmail.com>
2023-08-30 01:23:35 -0400

    Backup

--- src/main/java/com/gdblab/parser/RPQGrammarBaseListener.java
+++ src/main/java/com/gdblab/parser/RPQGrammarBaseListener.java
@@ -3,7 +3,10 @@ package com.gdblab.parser;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Pattern;
 
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
@@ -16,21 +19,49 @@ import com.gdblab.recursion.Recursion;
 import com.gdblab.schema.GraphObject;
 import com.gdblab.schema.Path;
 
+class TreeNode {
+	String label;
+	List<TreeNode> childs;
+
+	public TreeNode(String label) {
+		this.label = label;
+		this.childs = new ArrayList<>();
+	}
+
+	public void addChild(TreeNode child) {
+		childs.add(child);
+	}
+
+	public void addChilds(List<TreeNode> childs) {
+		this.childs.addAll(childs);
+	}
+
+	public TreeNode getChildByIndex(int index) {
+		return this.childs.get(index);
+	}
+
+	public int getChildsCount() {
+		return this.childs.size();
+	}
+}
+
 /**
  * InnerRPQGrammarBaseListener
  */
-class Eval{
+class Eval {
 	private String term;
 	private ArrayList<Path> paths;
 
-	public Eval(String term, ArrayList<Path> paths){
+	public Eval(String term, ArrayList<Path> paths) {
 		this.term = term;
 		this.paths = paths;
 	}
 }
+
 /**
  * This class provides an empty implementation of {@link RPQGrammarListener},
- * which can be extended to create a listener which only needs to handle a subset
+ * which can be extended to create a listener which only needs to handle a
+ * subset
  * of the available methods.
  */
 @SuppressWarnings("CheckReturnValue")
@@ -39,186 +70,245 @@ public class RPQGrammarBaseListener implements RPQGrammarListener {
 	private Database database;
 	private Integer MAX = 2;
 	private HashMap<String, ArrayList<Path>> evals = new HashMap<>();
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override public void enterQuery(RPQGrammarParser.QueryContext ctx) {
+	@Override
+	public void enterQuery(RPQGrammarParser.QueryContext ctx) {
 		this.database = new Database();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void exitQuery(RPQGrammarParser.QueryContext ctx) {
 		System.out.println("====================================================");
+		System.out.println("Query: " + ctx.getText());
+		System.out.println("====================================================");
+		TreeNode tree = this.buildtree(ctx.getChild(0));
+		this.printTree(tree, "");
 	}
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitQuery(RPQGrammarParser.QueryContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterExpression(RPQGrammarParser.ExpressionContext ctx) {
+
+	private TreeNode buildtree(ParseTree p) {
+		try {
+			TreeNode root = new TreeNode(p.getText());
+			List<TreeNode> childs = new ArrayList<>();
+			
+			for (int i = 0; i < p.getChildCount(); i++) {
+				if (!p.getChild(i).getText().equals(".")) {
+					childs.add(new TreeNode(p.getChild(i).getText()));
+				}
+			}
+
+			for (int i = 0; i < childs.size(); i++) {
+				childs.add(buildtree(p.getChild(i)));
+			}
+
+			root.addChilds(childs);
+
+			return root;
+		} catch (Exception e) {
+			return null;
+		}
 		
 	}
+
+	private void printTree(TreeNode node, String prefix) {
+		System.out.println(prefix + "└── " + node.label);
+
+		for (int i = 0; i < node.childs.size(); i++) {
+			if (i == node.childs.size() - 1) {
+				printTree(node.childs.get(i), prefix + "    ");
+			} else {
+				printTree(node.childs.get(i), prefix + "│   ");
+			}
+		}
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override public void exitExpression(RPQGrammarParser.ExpressionContext ctx) { }
+	@Override
+	public void enterExpression(RPQGrammarParser.ExpressionContext ctx) {
+
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override public void enterTerm(RPQGrammarParser.TermContext ctx) { }
+	@Override
+	public void exitExpression(RPQGrammarParser.ExpressionContext ctx) {
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override public void exitTerm(RPQGrammarParser.TermContext ctx) {
-		
+	@Override
+	public void enterTerm(RPQGrammarParser.TermContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void exitTerm(RPQGrammarParser.TermContext ctx) {
+
 		ArrayList<Path> paths = new ArrayList<>();
 		String term = ctx.getText();
-		System.out.println("Incoming term: " + term);
-		System.out.println("Saved terms: ");
-		for (String key : this.evals.keySet()) {
-			System.out.print(key + ", ");
-		}
-		System.out.println();
 
-		if (term.contains("(") && term.contains(")")) {
-			String operator = null; // este es el operador del conjunto
-			String lastChar = Character.toString(term.charAt(term.length() - 1));
-			if (lastChar.equals("?") || lastChar.equals("*") || lastChar.equals("+")) {
-				operator = lastChar;
-				term = term.substring(0, term.length() - 1);
-			}
-
-			if(term.chars().filter(c -> c == '(').count() == 1){
-				ArrayList<Path> joinPaths = new ArrayList<>();
-
-				term = term.substring(1, term.length() - 1);
-				ArrayList<String> terms = new ArrayList<String>(java.util.Arrays.asList(term.split("\\.")));
-				
-				for (String t : terms) {
-					System.out.println("T: " + t);
-					if(joinPaths.isEmpty()){
-						joinPaths = this.evals.get(t);
-						printPath(joinPaths);
-					}
-					else{
-						joinPaths = PathAlgebra.NodeJoin(joinPaths, this.evals.get(t));
-						printPath(joinPaths);
-					}
-				}
-
-				this.evals.put(term, joinPaths);
-				
-			}
-			else{
-				// ver que ****** hacer
-			}
-
-		}
-
-
-
-
-
-		// esta parte esta lista
-		else {
-			if(term.contains("+")){
+		if (!term.contains("(") && !term.contains(")")) {
+			if (term.contains("+")) {
 				String termReplaced = term.replaceAll("\\+", "");
 				this.evals.put(term, this.evaluatePlus(termReplaced));
-			}
-			else if (term.contains("*")) {
+			} else if (term.contains("*")) {
 				String termReplaced = term.replaceAll("\\*", "");
 				this.evals.put(term, this.evaluateKleene(termReplaced));
-			}
-			else if (term.contains("?")) {
+			} else if (term.contains("?")) {
 				String termReplaced = term.replaceAll("\\?", "");
 				this.evals.put(term, this.evaluateOptional(termReplaced));
-			}
-			else {
+			} else {
 				paths = Select.eval(this.database.graph.getPaths(), new Label(term, 1));
 				this.evals.put(term, paths);
 			}
 		}
-
-		// for (String key : this.evals.keySet()) {
-		// 	System.out.println("Key: " + key);
-		// 	printPath(this.evals.get(key));
-		// }
-		System.out.println("====================================================");
-
 	}
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterBase(RPQGrammarParser.BaseContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitBase(RPQGrammarParser.BaseContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterOperator(RPQGrammarParser.OperatorContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitOperator(RPQGrammarParser.OperatorContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterSeparator(RPQGrammarParser.SeparatorContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitSeparator(RPQGrammarParser.SeparatorContext ctx) { }
 
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override public void enterEveryRule(ParserRuleContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitEveryRule(ParserRuleContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void visitTerminal(TerminalNode node) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void visitErrorNode(ErrorNode node) { }
+	@Override
+	public void enterBase(RPQGrammarParser.BaseContext ctx) {
+	}
 
-	
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void exitBase(RPQGrammarParser.BaseContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void enterOperator(RPQGrammarParser.OperatorContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void exitOperator(RPQGrammarParser.OperatorContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void enterSeparator(RPQGrammarParser.SeparatorContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void exitSeparator(RPQGrammarParser.SeparatorContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void enterEveryRule(ParserRuleContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void exitEveryRule(ParserRuleContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void visitTerminal(TerminalNode node) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void visitErrorNode(ErrorNode node) {
+	}
+
 	private void printPath(ArrayList<Path> paths) {
 		for (Path pp : paths) {
 			System.out.print(pp.getId() + " : ");
@@ -228,16 +318,24 @@ public class RPQGrammarBaseListener implements RPQGrammarListener {
 		}
 	}
 
-	
-	
-	private ArrayList<Path> evaluatePlus(String label){
+	private void printEvals() {
+		System.out.println("====================================================");
+		System.out.println("Evaluations: ");
+		for (String key : this.evals.keySet()) {
+			System.out.println(key + " : ");
+			this.printPath(this.evals.get(key));
+		}
+		System.out.println("====================================================");
+	}
+
+	private ArrayList<Path> evaluatePlus(String label) {
 		ArrayList<Path> paths = new ArrayList<>();
 		paths = Select.eval(this.database.graph.getPaths(), new Label(label, 1));
 		paths = Recursion.arbitrary(paths, this.MAX);
 		return paths;
 	}
 
-	private ArrayList<Path> evaluateKleene(String label){
+	private ArrayList<Path> evaluateKleene(String label) {
 		ArrayList<Path> paths = new ArrayList<>();
 		paths = Select.eval(this.database.graph.getPaths(), new Label(label, 1));
 		paths = Recursion.arbitrary(paths, this.MAX);
@@ -245,7 +343,7 @@ public class RPQGrammarBaseListener implements RPQGrammarListener {
 		return paths;
 	}
 
-	private ArrayList<Path> evaluateOptional(String label){
+	private ArrayList<Path> evaluateOptional(String label) {
 		ArrayList<Path> paths = new ArrayList<>();
 		paths = Select.eval(this.database.graph.getPaths(), new Label(label, 1));
 		paths = PathAlgebra.Union(paths, this.database.getPathsWithoutEdges());
