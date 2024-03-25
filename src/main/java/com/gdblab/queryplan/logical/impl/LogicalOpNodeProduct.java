fed71e24d923520c615454dddc34ff252d98d887
Sebastián Ferrada <scferradaa@gmail.com>
2024-03-25 09:18:24 -0300

    Update src/main/java/com/gdblab/queryplan/logical/impl/LogicalOpNodeProduct.java

--- src/main/java/com/gdblab/queryplan/logical/impl/LogicalOpNodeProduct.java
+++ src/main/java/com/gdblab/queryplan/logical/impl/LogicalOpNodeProduct.java
@@ -5,7 +5,7 @@ import com.gdblab.queryplan.logical.LogicalPlanVisitor;
 public class LogicalOpNodeProduct extends BinaryLogicalOp {
 
     @Override
-    public void acceptVisitor(LogicalPlanVisitor visitor) {
+    public void acceptVisitor(final LogicalPlanVisitor visitor) {
         visitor.visit(this);
     }
 }
