a5d231d8633ca99bd4a1792814579fa4609598b1
Sebastián Ferrada <scferradaa@gmail.com>
2024-03-25 09:18:07 -0300

    Update src/main/java/com/gdblab/queryplan/logical/LogicalOperator.java

--- src/main/java/com/gdblab/queryplan/logical/LogicalOperator.java
+++ src/main/java/com/gdblab/queryplan/logical/LogicalOperator.java
@@ -5,6 +5,6 @@ public interface LogicalOperator {
     /**
      * Receives a Logical Plan visitor that traverses the logical plan tree
      */
-    void acceptVisitor(LogicalPlanVisitor visitor );
+    void acceptVisitor(final LogicalPlanVisitor visitor );
 
 }
