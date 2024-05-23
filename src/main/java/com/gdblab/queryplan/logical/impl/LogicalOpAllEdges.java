07133e42b722b6fd24fba6e1e542993565faf5ac
Vicente Rojas <vrojasaranda@gmail.com>
2024-05-23 09:34:44 -0400

    Finish OpAllEdges
    Add all edges physical operator and change the access to the database object to getInstance()

--- src/main/java/com/gdblab/queryplan/logical/impl/LogicalOpAllEdges.java
+++ src/main/java/com/gdblab/queryplan/logical/impl/LogicalOpAllEdges.java
@@ -2,8 +2,16 @@ package com.gdblab.queryplan.logical.impl;
 
 import com.gdblab.queryplan.logical.LogicalPlanVisitor;
 import com.gdblab.queryplan.logical.NullaryLogicalOperator;
+import com.gdblab.schema.impl.MemoryGraph;
 
 public class LogicalOpAllEdges implements NullaryLogicalOperator {
+    
+    private MemoryGraph graph;
+
+    public LogicalOpAllEdges() {
+        this.graph = MemoryGraph.getInstance();
+    }
+    
     @Override
     public void acceptVisitor(final LogicalPlanVisitor visitor) {
         visitor.visit(this);
@@ -17,5 +25,13 @@ public class LogicalOpAllEdges implements NullaryLogicalOperator {
     @Override
     public String toString() {
         return "S1";
+    }
+
+    public MemoryGraph getGraph() {
+        return graph;
+    }
+
+    public void setGraph(MemoryGraph graph) {
+        this.graph = graph;
     }
 }
