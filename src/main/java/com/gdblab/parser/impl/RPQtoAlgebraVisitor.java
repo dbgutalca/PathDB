604789ca3e83964035d46cabdcdcc02227780e29
Vicente Rojas <vrojasaranda@gmail.com>
2024-05-28 12:48:53 -0400

    Add negated label block

--- src/main/java/com/gdblab/parser/impl/RPQtoAlgebraVisitor.java
+++ src/main/java/com/gdblab/parser/impl/RPQtoAlgebraVisitor.java
@@ -1,6 +1,7 @@
 package com.gdblab.parser.impl;
 
 import com.gdblab.algebra.condition.Label;
+import com.gdblab.algebra.condition.Not;
 import com.gdblab.parser.RPQExpressionVisitor;
 import com.gdblab.queryplan.logical.LogicalOperator;
 import com.gdblab.queryplan.logical.impl.*;
@@ -53,7 +54,10 @@ public class RPQtoAlgebraVisitor implements RPQExpressionVisitor {
 
     @Override
     public void visit(final LabelExpression labelExpression) {
-        stack.push(new LogicalOpSelection(new LogicalOpAllEdges(), new Label(labelExpression.getLabel(), 1)));
+        if (labelExpression.getLabel().startsWith("!")) 
+            stack.push(new LogicalOpSelection(new LogicalOpAllEdges(), new Not(new Label( labelExpression.getLabel().substring(1), 1))));
+        else
+            stack.push(new LogicalOpSelection(new LogicalOpAllEdges(), new Label( labelExpression.getLabel(), 1)));
     }
 
     public LogicalOperator getRoot() {
