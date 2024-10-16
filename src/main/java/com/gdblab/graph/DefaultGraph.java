1bc8a57e7300aa34c89a2ad13d63752d9037a96c
Vicente Rojas <vrojasaranda@gmail.com>
2024-10-16 12:26:07 -0300

    Reorganize default graph

---
+++ src/main/java/com/gdblab/graph/DefaultGraph.java
@@ -0,0 +1,41 @@
+package com.gdblab.graph;
+
+import com.gdblab.graph.interfaces.InterfaceGraph;
+import com.gdblab.graph.schema.*;
+
+public final class DefaultGraph {
+
+    private static InterfaceGraph graph;
+
+    public static void loadGraph() {
+        graph = Graph.getGraph();
+
+        Node[] nodes = getDefaultNodes();
+        for (Node n : nodes) {
+            graph.insertNode(n);
+        }
+
+        Edge[] edges = getDefaultEdges();
+        for (Edge e : edges) {
+            graph.insertEdge(e);
+        }
+    }
+    
+    private static Node[] getDefaultNodes(){
+        Node[] nodes = new Node[3];
+        nodes[0] = new Node("N1", "N1");
+        nodes[1] = new Node("N2", "N2");
+        nodes[2] = new Node("N3", "N3");
+        return nodes;
+    }
+
+    private static Edge[] getDefaultEdges() {
+        Edge[] edges = new Edge[5];
+        edges[0] = new Edge("E1", "A", graph.getNode("N1"), graph.getNode("N2"));
+        edges[1] = new Edge("E2", "B", graph.getNode("N2"), graph.getNode("N2"));
+        edges[2] = new Edge("E3", "D", graph.getNode("N2"), graph.getNode("N3"));
+        edges[3] = new Edge("E4", "C", graph.getNode("N2"), graph.getNode("N2"));
+        edges[4] = new Edge("E5", "A", graph.getNode("N3"), graph.getNode("N2"));
+        return edges;
+    }
+}
