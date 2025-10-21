ec8aef40fc3dd900c9f35aafb8946aac5a73a31b
Vicente Rojas <virojas15@alumnos.utalca.cl>
2025-10-21 11:28:23 -0300

    Fix default graph

--- src/main/java/com/gdblab/graph/DefaultGraph.java
+++ src/main/java/com/gdblab/graph/DefaultGraph.java
@@ -3,27 +3,60 @@ package com.gdblab.graph;
 import java.util.HashMap;
 
 import com.gdblab.graph.interfaces.InterfaceGraph;
-import com.gdblab.graph.schema.*;
+import com.gdblab.graph.schema.Edge;
+import com.gdblab.graph.schema.Node;
 
 public final class DefaultGraph {
 
     private static InterfaceGraph graph = Graph.getGraph();
-    
-    public static Node[] getDefaultNodes(){
-        Node[] nodes = new Node[3];
-        // nodes[0] = new Node("N1", "N1");
-        // nodes[1] = new Node("N2", "N2");
-        // nodes[2] = new Node("N3", "N3");
+
+    public static Node[] getDefaultNodes() {
+        Node[] nodes = new Node[7];
+
+        HashMap<String, String> m_1_property = new HashMap<>();
+        m_1_property.put("txt", "Msg1");
+        nodes[0] = new Node("m1", "Message", m_1_property);
+
+        HashMap<String, String> m_2_property = new HashMap<>();
+        m_2_property.put("txt", "Msg2");
+        nodes[1] = new Node("m2", "Message", m_2_property);
+
+        HashMap<String, String> m_3_property = new HashMap<>();
+        m_3_property.put("txt", "Msg3");
+        nodes[2] = new Node("m3", "Message", m_3_property);
+
+        HashMap<String, String> p_1_property = new HashMap<>();
+        p_1_property.put("name", "Moe");
+        nodes[3] = new Node("p1", "Person", p_1_property);
+
+        HashMap<String, String> p_2_property = new HashMap<>();
+        p_2_property.put("name", "Bart");
+        nodes[4] = new Node("p2", "Person", p_2_property);
+
+        HashMap<String, String> p_3_property = new HashMap<>();
+        p_3_property.put("name", "Lisa");
+        nodes[5] = new Node("p3", "Person", p_3_property);
+
+        HashMap<String, String> p_4_property = new HashMap<>();
+        p_4_property.put("name", "Apu");
+        nodes[6] = new Node("p4", "Person", p_4_property);
+
         return nodes;
     }
 
     public static Edge[] getDefaultEdges() {
-        Edge[] edges = new Edge[5];
-        edges[0] = new Edge("E1", "A", graph.getNode("N1"), graph.getNode("N2"), new HashMap<String, String>());
-        edges[1] = new Edge("E2", "A", graph.getNode("N2"), graph.getNode("N2"), new HashMap<String, String>());
-        edges[2] = new Edge("E3", "B", graph.getNode("N2"), graph.getNode("N3"), new HashMap<String, String>());
-        edges[3] = new Edge("E4", "A", graph.getNode("N1"), graph.getNode("N3"), new HashMap<String, String>());
-        edges[4] = new Edge("E5", "B", graph.getNode("N3"), graph.getNode("N1"), new HashMap<String, String>());
+        Edge[] edges = new Edge[11];
+        edges[0] = new Edge("E1", "knows", graph.getNode("p1"), graph.getNode("p2"), new HashMap<String, String>());
+        edges[1] = new Edge("E2", "knows", graph.getNode("p2"), graph.getNode("p3"), new HashMap<String, String>());
+        edges[2] = new Edge("E3", "knows", graph.getNode("p3"), graph.getNode("p2"), new HashMap<String, String>());
+        edges[3] = new Edge("E4", "knows", graph.getNode("p2"), graph.getNode("p4"), new HashMap<String, String>());
+        edges[4] = new Edge("E5", "likes", graph.getNode("p2"), graph.getNode("m3"), new HashMap<String, String>());
+        edges[5] = new Edge("E6", "likes", graph.getNode("p4"), graph.getNode("m3"), new HashMap<String, String>());
+        edges[6] = new Edge("E7", "likes", graph.getNode("p3"), graph.getNode("m2"), new HashMap<String, String>());
+        edges[7] = new Edge("E8", "likes", graph.getNode("p1"), graph.getNode("m1"), new HashMap<String, String>());
+        edges[8] = new Edge("E9", "hasCreator", graph.getNode("m3"), graph.getNode("p1"), new HashMap<String, String>());
+        edges[9] = new Edge("E10", "hasCreator", graph.getNode("m2"), graph.getNode("p4"), new HashMap<String, String>());
+        edges[10] = new Edge("E11", "hasCreator", graph.getNode("m1"), graph.getNode("p3"), new HashMap<String, String>());
         return edges;
     }
 }
