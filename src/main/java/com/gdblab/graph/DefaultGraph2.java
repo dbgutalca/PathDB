b311598f587a60c0dc9ee8444a3c644402e2ef54
Vicente Rojas <virojas15@alumnos.utalca.cl>
2025-10-21 09:50:06 -0300

    fix: Change case sensitive default edges label

--- src/main/java/com/gdblab/graph/DefaultGraph2.java
+++ src/main/java/com/gdblab/graph/DefaultGraph2.java
@@ -3,13 +3,14 @@ package com.gdblab.graph;
 import java.util.HashMap;
 
 import com.gdblab.graph.interfaces.InterfaceGraph;
-import com.gdblab.graph.schema.*;
+import com.gdblab.graph.schema.Edge;
+import com.gdblab.graph.schema.Node;
 
 public final class DefaultGraph2 {
 
     private static InterfaceGraph graph = Graph.getGraph();
-    
-    public static Node[] getDefaultNodes(){
+
+    public static Node[] getDefaultNodes() {
         Node[] nodes = new Node[7];
 
         HashMap<String, String> m_1_property = new HashMap<>();
@@ -40,23 +41,22 @@ public final class DefaultGraph2 {
         p_4_property.put("name", "Apu");
         nodes[6] = new Node("p4", "Person", p_4_property);
 
-
         return nodes;
     }
 
     public static Edge[] getDefaultEdges() {
         Edge[] edges = new Edge[11];
-        edges[0] = new Edge("E1", "Knows", graph.getNode("p1"), graph.getNode("p2"), new HashMap<String, String>());
-        edges[1] = new Edge("E2", "Knows", graph.getNode("p2"), graph.getNode("p3"), new HashMap<String, String>());
-        edges[2] = new Edge("E3", "Knows", graph.getNode("p3"), graph.getNode("p2"), new HashMap<String, String>());
-        edges[3] = new Edge("E4", "Knows", graph.getNode("p2"), graph.getNode("p4"), new HashMap<String, String>());
-        edges[4] = new Edge("E5", "Likes", graph.getNode("p2"), graph.getNode("m3"), new HashMap<String, String>());
-        edges[5] = new Edge("E6", "Likes", graph.getNode("p4"), graph.getNode("m3"), new HashMap<String, String>());
-        edges[6] = new Edge("E7", "Likes", graph.getNode("p3"), graph.getNode("m2"), new HashMap<String, String>());
-        edges[7] = new Edge("E8", "Likes", graph.getNode("p1"), graph.getNode("m1"), new HashMap<String, String>());
-        edges[8] = new Edge("E9", "HasCreator", graph.getNode("m3"), graph.getNode("p1"), new HashMap<String, String>());
-        edges[9] = new Edge("E10", "HasCreator", graph.getNode("m2"), graph.getNode("p4"), new HashMap<String, String>());
-        edges[10] = new Edge("E11", "HasCreator", graph.getNode("m1"), graph.getNode("p3"), new HashMap<String, String>());
+        edges[0] = new Edge("E1", "knows", graph.getNode("p1"), graph.getNode("p2"), new HashMap<String, String>());
+        edges[1] = new Edge("E2", "knows", graph.getNode("p2"), graph.getNode("p3"), new HashMap<String, String>());
+        edges[2] = new Edge("E3", "knows", graph.getNode("p3"), graph.getNode("p2"), new HashMap<String, String>());
+        edges[3] = new Edge("E4", "knows", graph.getNode("p2"), graph.getNode("p4"), new HashMap<String, String>());
+        edges[4] = new Edge("E5", "likes", graph.getNode("p2"), graph.getNode("m3"), new HashMap<String, String>());
+        edges[5] = new Edge("E6", "likes", graph.getNode("p4"), graph.getNode("m3"), new HashMap<String, String>());
+        edges[6] = new Edge("E7", "likes", graph.getNode("p3"), graph.getNode("m2"), new HashMap<String, String>());
+        edges[7] = new Edge("E8", "likes", graph.getNode("p1"), graph.getNode("m1"), new HashMap<String, String>());
+        edges[8] = new Edge("E9", "hasCreator", graph.getNode("m3"), graph.getNode("p1"), new HashMap<String, String>());
+        edges[9] = new Edge("E10", "hasCreator", graph.getNode("m2"), graph.getNode("p4"), new HashMap<String, String>());
+        edges[10] = new Edge("E11", "hasCreator", graph.getNode("m1"), graph.getNode("p3"), new HashMap<String, String>());
         return edges;
     }
 }
